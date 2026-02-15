1aaa7e0f35595d9abb6ede4acbc93e01fd7ed584
NathanMuljadi <nathanjmuljadi@gmail.com>
2026-02-15 15:44:14 -0800

    Implemented Person class

---
+++ Person.py
@@ -0,0 +1,8 @@
+class Person():
+    def __init__(self, yearBorn, yearDied, firstName, lastName, spouse, children):
+        self.yearBorn = yearBorn
+        self.yearDied = yearDied
+        self.firstName = firstName
+        self.lastName = lastName
+        self.spouse = spouse
+        self.children = children
