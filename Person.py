604a00d4d0136edb75dfd87d4118211c76f2e9f9
NathanMuljadi <nathanjmuljadi@gmail.com>
2026-02-15 16:07:43 -0800

    Renamed variables

--- Person.py
+++ Person.py
@@ -1,8 +1,8 @@
 class Person():
-    def __init__(self, yearBorn, yearDied, firstName, lastName, spouse, children):
-        self.yearBorn = yearBorn
-        self.yearDied = yearDied
-        self.firstName = firstName
-        self.lastName = lastName
+    def __init__(self, year_born, year_died, first_name, last_name, spouse, children=[]):
+        self.year_born = year_born
+        self.year_died = year_died
+        self.first_name = first_name
+        self.last_name = last_name
         self.spouse = spouse
         self.children = children
